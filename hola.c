54edbae534a664e2d6eb9c35439769a6644970c1
root <gescamillan@gmail.com>
2014-01-26 16:39:44 -0800

    quinta version del hola mundo

--- hola.c
+++ hola.c
@@ -1,4 +1,22 @@
 #include <stdio.h>
+#include "test/test.h"
+
+void main(void) {
+	test();
+	printf("Hola Mundo\n");
+}
+
+void funcion2(void) {
+	printf("Funcion 2\n")
+}
+
+void funcion1(void) {
+	printf("Funcion 1\n");
+}
+
+void funcion3(void) {
+	printf("Funcion 3\n");
+}#include <stdio.h>
 
 void main(void) {
 	printf("Hola Mundo\n");
